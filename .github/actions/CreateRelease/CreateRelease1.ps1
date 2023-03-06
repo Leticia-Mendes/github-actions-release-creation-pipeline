ecc6c8f186851448350efa7d60f9361eeef84f40
Leticia-Mendes <90715443+Leticia-Mendes@users.noreply.github.com>
2023-03-05 21:13:37 -0300

    Update CreateRelease1.ps1

--- .github/actions/CreateRelease/CreateRelease1.ps1
+++ .github/actions/CreateRelease/CreateRelease1.ps1
@@ -2,33 +2,54 @@ $release_name = $env:release_name
 $token = $env:token
 $user = "leticiaaraujo-mcd"
 
-Install-Module -Name PowerShellForGitHub -Force
 
-$secureString = ($token | ConvertTo-SecureString -AsPlainText -Force)
-    $cred = New-Object System.Management.Automation.PSCredential "username is ignored", $secureString
-    Set-GitHubAuthentication -Credential $cred
-    $secureString = $null # clear this out now that it's no longer needed
-    $cred = $null # clear this out now that it's no longer needed
+function Main {
+	Get-Modules 
+  Get-Credentials
+	$releaseVersionsValues = Get-ReleaseVersionsFile
+	GenerateRelease $releaseVersionsValues
+}
 
-$releaseVersions = Get-Content -Path .\release-versions.yml
-foreach ($line in $releaseVersions) {	
-	if ($line -match $release_name)
-	{
-		Write-Host "[$line ] in release-versions.yml matches release_name input [$release_name]"
+function Get-Modules{
+	Install-Module powershell-yaml -Force
+	Install-Module -Name PowerShellForGitHub -Force 
+}
 
-		$line = ($line -replace '\s+').split(":")
-		$repo = $line[0]
-		$tag = $line[1]
-		Write-Host "repo: " $repo
-		Write-Host "tag: " $tag
-		
+function Get-Credentials {
+	$secureString = ($token | ConvertTo-SecureString -AsPlainText -Force)
+	$cred = New-Object System.Management.Automation.PSCredential "username is ignored", $secureString
+	Set-GitHubAuthentication -Credential $cred
+	$secureString = $null # clear this out now that it's no longer needed
+	$cred = $null # clear this out now that it's no longer needed
+}
+
+function Get-ReleaseVersionsFile {
+	$fileReleaseVersions = Get-Content -Path ".\release-versions.yml"
+	$content = ''
+	foreach ($line in $fileReleaseVersions) { 
+		$content = $content + "`n" + $line 
+	}
+	$releaseVersions = ConvertFrom-YAML $content
+	$releaseVersionsValues = $releaseVersions.Values
+	return $releaseVersionsValues	
+}
+
+function GenerateRelease($releaseVersionsValues) {
+	foreach ($repo in $releaseVersionsValues.Keys) {	
+		$tag = $releaseVersionsValues.Item($repo)
+		Write-Host "RepoName: " $repo " - Tag: " $tag
 		if ($tag -eq "latest") {
 			$latestTag = Get-GitHubRelease -OwnerName $user -RepositoryName $repo -Latest
 			$tag = $latestTag.tag_name
-			Write-Host "the latest tag is $tag"
-			$tag = "12345"
-			Write-Host $tag
+			Write-Host "The latest tag for the repository $repo is $tag"
+			$tag = $tag + ".0"
+			write-host "The new tag for the repository $repo is $tag"
 		}
-		New-GitHubRelease -OwnerName $user -RepositoryName $repo -Tag $tag -Body "Message here"
+	
+		New-GitHubRelease -OwnerName $user -RepositoryName $repo -Name $release_name -Tag $tag -Body "Message here :) "
+		Write-Host "The $release_name release was generated with $tag tag for the $repo repository."
+		Write-Host "-------------------------------"
 	}
 }
+
+Main
